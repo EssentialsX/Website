297e46cf2eb623aa6aa98e5d30af3af4bf15b882
md678685 <1917406+md678685@users.noreply.github.com>
2019-06-23 17:49:34 +0100

    Improve sidebar appearance

--- src/components/WikiMenu.vue
+++ src/components/WikiMenu.vue
@@ -1,12 +1,13 @@
 <template>
-    <aside class="menu">
+    <aside class="menu box">
         <template v-for="(contents, title) in $themeConfig.wikiSidebar">
-            <!-- TODO: fix the spacing by putting all elements under one  -->
             <p class="menu-label" :key="title + '_label'">{{ title }}</p>
             <ul class="menu-list" :key="title + '_list'">
                 <li v-for="item in contents" :key="item.label">
                     <SaberLink v-if="item.link" :to="item.link" :class="linkClass(item.link)">{{ item.label }}</SaberLink>
-                    <a v-else-if="item.href" :href="item.href">{{ item.label }}</a>
+                    <a v-else-if="item.href" :href="item.href">
+                        {{ item.label }}
+                    </a>
                     <ul v-if="item.children">
                         <li v-for="subitem in item.children" :key="subitem.label">
                             <SaberLink v-if="subitem.link" :to="subitem.link" :class="linkClass(subitem.link)">{{ subitem.label }}</SaberLink>
