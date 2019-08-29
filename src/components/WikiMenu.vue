8cb67859a700a16d262855a03c70ea5cfe793593
md678685 <1917406+md678685@users.noreply.github.com>
2019-08-29 17:01:27 +0100

    Make wiki menu toggleable

--- src/components/WikiMenu.vue
+++ src/components/WikiMenu.vue
@@ -1,23 +1,41 @@
 <template>
-    <aside class="menu box tilt">
-        <h2 class="subtitle">Pages</h2>
-        <template v-for="(contents, title) in $themeConfig.wikiSidebar">
-            <p class="menu-label" :key="title + '_label'">{{ title }}</p>
-            <ul class="menu-list" :key="title + '_list'">
-                <li v-for="item in contents" :key="item.label">
-                    <SaberLink v-if="item.link" :to="item.link" :class="linkClass(item.link)">{{ item.label }}</SaberLink>
-                    <a v-else-if="item.href" :href="item.href" target="_blank">
-                        {{ item.label }}
-                    </a>
-                    <ul v-if="item.children">
-                        <li v-for="subitem in item.children" :key="subitem.label">
-                            <SaberLink v-if="subitem.link" :to="subitem.link" :class="linkClass(subitem.link)">{{ subitem.label }}</SaberLink>
+    <b-collapse class="card tilt">
+        <div
+            slot="trigger"
+            slot-scope="props"
+            class="card-header"
+            role="button">
+            <p class="card-header-title">
+                Pages
+            </p>
+            <a class="card-header-icon">
+                <b-icon
+                    size="is-small"
+                    :icon="props.open ? 'chevron-up' : 'chevron-down'">
+                </b-icon>
+            </a>
+        </div>
+        <div class="card-content">
+            <aside class="menu">
+                <template v-for="(contents, title) in $themeConfig.wikiSidebar">
+                    <p class="menu-label" :key="title + '_label'">{{ title }}</p>
+                    <ul class="menu-list" :key="title + '_list'">
+                        <li v-for="item in contents" :key="item.label">
+                            <SaberLink v-if="item.link" :to="item.link" :class="linkClass(item.link)">{{ item.label }}</SaberLink>
+                            <a v-else-if="item.href" :href="item.href" target="_blank">
+                                {{ item.label }}
+                            </a>
+                            <ul v-if="item.children">
+                                <li v-for="subitem in item.children" :key="subitem.label">
+                                    <SaberLink v-if="subitem.link" :to="subitem.link" :class="linkClass(subitem.link)">{{ subitem.label }}</SaberLink>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
-                </li>
-            </ul>
-        </template>
-    </aside>
+                </template>
+            </aside>
+        </div>
+    </b-collapse>
 </template>
 
 <script>
