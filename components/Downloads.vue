18e79a80f82c1d28e82fdab2d69fbc882c1daa38
md678685 <1917406+md678685@users.noreply.github.com>
2020-01-10 09:30:45 +0000

    Update downloads page

--- components/Downloads.vue
+++ components/Downloads.vue
@@ -2,7 +2,7 @@
     <section class="section">
         <div class="box content">
             <p class="tip">
-                Not sure what the different jars do? See the <SaberLink to="/wiki/Downloading-EssentialsX.html">downloads guide</SaberLink>.
+                Not sure what to download? See the <SaberLink to="/wiki/Installing-EssentialsX.html">Installing EssentialsX guide</SaberLink> and <SaberLink to="/wiki/Module-Breakdown.html">module breakdown</SaberLink>.
             </p>
             <p v-if="loading">
                 <i>Currently loading downloads, please wait...</i>
@@ -29,13 +29,13 @@
             <div class="tile is-parent is-vertical is-3">
                 <downloads-tile
                     text="AntiBuild"
-                    description="Permissions for building"
+                    description="Restrict building with permissions"
                     :version="version"
                     :url="plugins['EssentialsX AntiBuild'].main"
                 />
                 <downloads-tile
                     text="Protect"
-                    description="World and gameplay rules"
+                    description="Configurable world protection"
                     :version="version"
                     :url="plugins['EssentialsX Protect'].main"
                 />
@@ -43,13 +43,13 @@
             <div class="tile is-parent is-vertical is-3">
                 <downloads-tile
                     text="Chat"
-                    description="Chat formatting + local chat"
+                    description="Chat formatting & local chat"
                     :version="version"
                     :url="plugins['EssentialsX Chat'].main"
                 />
                 <downloads-tile
                     text="Spawn"
-                    description="Spawn control"
+                    description="Fine-grained spawnpoint control"
                     :version="version"
                     :url="plugins['EssentialsX Spawn'].main"
                 />
@@ -57,13 +57,13 @@
             <div class="tile is-parent is-vertical is-3">
                 <downloads-tile
                     text="GeoIP"
-                    description="Geographic lookup"
+                    description="Geographic player lookup"
                     :version="version"
                     :url="plugins['EssentialsX GeoIP'].main"
                 />
                 <downloads-tile
                     text="XMPP"
-                    description="Jabber chat integration"
+                    description="Jabber server integration"
                     :version="version"
                     :url="plugins['EssentialsX XMPP'].main"
                 />
