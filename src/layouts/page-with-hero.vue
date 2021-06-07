eefacfd7a137b0893d4ed2d64f698c3fae3ab5c3
MD <1917406+mdcfe@users.noreply.github.com>
2021-06-07 17:19:24 +0100

    Add page-with-hero layout

---
+++ src/layouts/page-with-hero.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">{{ heroTitle }}</h1>
+                    <h2 class="subtitle">{{ heroSubtitle }}</h2>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="container">
+                <div class="content">
+                    <slot name="default" />
+                </div>
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
+
+export default {
+    props: ['page'],
+    components: {
+        Navbar,
+        Footer
+    },
+    head() {
+        return {
+            title: `${this.page.attributes.title} - ${this.$siteConfig.title}`
+        }
+    },
+    computed: {
+        heroTitle() {
+            return this.page.attributes.heroTitle || this.page.attributes.title;
+        },
+        heroSubtitle() {
+            return this.page.attributes.heroSubtitle || this.page.attributes.subtitle;
+        }
+    }
+}
+</script>
