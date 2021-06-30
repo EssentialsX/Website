9dd0a4f1b4514edb0deb4b9c2a65e1accb0f1dbf
MD <1917406+mdcfe@users.noreply.github.com>
2021-06-30 23:10:21 +0100

    netlify pls

--- plugins/wikilinks/index.js
+++ plugins/wikilinks/index.js
@@ -4,8 +4,10 @@ module.exports = {
     name: ID,
     apply: async (api, options) => {
         const wikilinksPlugin = (await import("@essentialsx/markdown-it-wikilinks")).default;
+        console.log(wikilinksPlugin)
 
         api.hooks.chainMarkdown.tap(ID, config => {
+            console.log("markdown tap")
             config.plugin(ID).use(wikilinksPlugin, [{
                 baseURL: "/wiki/"
             }]);
