d0de765ce742e2b3b896084da5276bfa8408061a
MD <1917406+mdcfe@users.noreply.github.com>
2021-10-25 01:59:14 +0100

    Minor visual changes

--- layouts/docs-v2.vue
+++ layouts/docs-v2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="overflow-x-hidden">
     <Navbar />
 
     <Hero>
--- layouts/no-container.vue
+++ layouts/no-container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="overflow-x-hidden">
     <Navbar />
 
     <section class="main-content">
