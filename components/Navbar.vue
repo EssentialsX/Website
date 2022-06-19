a1751d6841d54ac2deb4665f44b7154b9e213065
MD <1917406+mdcfe@users.noreply.github.com>
2022-06-19 16:46:09 +0100

    Fix mobile navbar icon positioning

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,7 +3,11 @@
     <div class="w-screen py-4 bg-gray-900 text-white">
       <div class="flex items-center container mx-auto h-full px-4">
         <NuxtLink to="/">
-          <img class="h-6" src="@/assets/images/logo-navbar.png" />
+          <img
+            class="h-6"
+            alt="EssentialsX logo"
+            src="@/assets/images/logo-navbar.png"
+          />
         </NuxtLink>
         <span class="flex-grow"></span>
         <span class="hidden md:inline items-center">
@@ -19,31 +23,47 @@
             <fa-icon :icon="themeIcon" />
           </a>
         </span>
-        <span class="md:hidden px-2">
-          <a class="px-3 py-2" @click="toggleTheme">
+        <span class="md:hidden pl-2">
+          <a class="px-2 py-2" @click="toggleTheme">
             <fa-icon :icon="themeIcon" />
           </a>
-          <a class="px-3 py-2 ml-2" @click="toggleMenu">
-            <fa-icon :icon="open ? 'caret-up' : 'bars'" />
+          <a class="px-2 py-2 ml-2" @click="toggleMenu">
+            <fa-icon :icon="open ? 'caret-up' : 'bars'" fixed-width />
           </a>
         </span>
       </div>
     </div>
     <div v-if="open" class="md:hidden p-2 bg-gray-800 text-white">
       <div class="container mx-auto flex flex-col">
-        <NavbarItem tag="NuxtLink" to="/downloads" class="flex flex-row items-center gap-2">
+        <NavbarItem
+          tag="NuxtLink"
+          to="/downloads"
+          class="flex flex-row items-center gap-2"
+        >
           <fa-icon icon="download" fixed-width />
           <span>Downloads</span>
         </NavbarItem>
-        <NavbarItem tag="NuxtLink" to="/docs" class="flex flex-row items-center gap-2">
+        <NavbarItem
+          tag="NuxtLink"
+          to="/docs"
+          class="flex flex-row items-center gap-2"
+        >
           <fa-icon icon="book" fixed-width />
           <span>Docs</span>
         </NavbarItem>
-        <NavbarItem tag="NuxtLink" to="/misc/get-help" class="flex flex-row items-center gap-2">
+        <NavbarItem
+          tag="NuxtLink"
+          to="/misc/get-help"
+          class="flex flex-row items-center gap-2"
+        >
           <fa-icon icon="life-ring" fixed-width />
           <span>Get help</span>
         </NavbarItem>
-        <NavbarItem tag="NuxtLink" to="/misc/support-us" class="flex flex-row items-center gap-2">
+        <NavbarItem
+          tag="NuxtLink"
+          to="/misc/support-us"
+          class="flex flex-row items-center gap-2"
+        >
           <fa-icon icon="heart" fixed-width />
           <span>Support us</span>
         </NavbarItem>
@@ -54,6 +74,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({
   data() {
     return {
