31ac37e8ab0ebf3c11f72f99f221cdfe9fc080f9
MD <1917406+mdcfe@users.noreply.github.com>
2022-06-21 16:07:42 +0100

    Fix docs navigation spacing on mobile

--- components/docs/sidebar/View.vue
+++ components/docs/sidebar/View.vue
@@ -3,7 +3,7 @@
     <section
       v-for="(contents, title) in sidebarMenu"
       :key="`sidebar_section_${title}`"
-      class="m-4 lg:ml-0 first-of-type:mt-0"
+      class="m-4 lg:(ml-0 first-of-type:mt-0)"
     >
       <p class="font-semibold text-lg mb-1">{{ title }}</p>
       <DocsSidebarMenuList :entries="contents" />
