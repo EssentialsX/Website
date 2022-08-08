2bcf6ff3f3704ea398fb8c2c67ccaf7c03e44bc8
MD <1917406+mdcfe@users.noreply.github.com>
2022-08-08 17:03:41 +0100

    Add tools index page

--- pages/tools/index.vue
+++ pages/tools/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>wooyeah</div>
+  <div>
+    <div class="container mx-auto px-4 py-2">
+      <div class="prose">
+        <p>
+          EssentialsX has web tools that you can launch from in-game and access
+          in your browser.
+        </p>
+        <ul>
+          <li>
+            <nuxt-link to="/tools/dump"><b>/ess dump</b> viewer</nuxt-link>:
+            View server data in your browser and share info for support
+            purposes.
+          </li>
+          <!--
+          <li class="text-strikethrough">
+            <nuxt-link to="/tools/locale"
+              ><b>messages.properties</b> viewer</nuxt-link
+            >: Download the latest translation files for EssentialsX.
+          </li>
+          <li class="text-strikethrough">
+            <nuxt-link to="/tools/config">Config editor</nuxt-link>: Edit your
+            EssentialsX configuration in the browser.
+          </li>
+          -->
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
