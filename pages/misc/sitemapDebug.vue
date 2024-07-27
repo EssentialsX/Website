4d708abe7a38f4241b9c8066d0d7e2f204b225e2
MD <1917406+mdcfe@users.noreply.github.com>
2024-07-27 19:23:59 +0100

    Make sitemap slightly nicer

--- pages/misc/sitemapDebug.vue
+++ pages/misc/sitemapDebug.vue
@@ -1,14 +1,36 @@
 <template>
   <NuxtLayout>
-    <div class="font-mono">
-      <code>
-        <pre>{{ articles }}</pre>
-      </code>
-    </div>
+    <KitContainer>
+      <KitButtons>
+        <KitButton @click="showRaw = !showRaw">Toggle raw view</KitButton>
+      </KitButtons>
+      <div class="font-mono bg-dark text-white rounded-lg p-4" v-if="showRaw">
+        <code>
+          <pre>{{ articles }}</pre>
+        </code>
+      </div>
+      <table class="table" v-else>
+        <thead>
+          <tr class="font-bold">
+            <td>Title</td>
+            <td>ID</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="article in articles">
+            <td class="underline"><NuxtLink :to="article._path">{{ article.title }}</NuxtLink></td>
+            <td>{{ article._id }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </KitContainer>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-const articles = await useAsyncData('home', () => queryContent().find())
+const { data: articles } = await useAsyncData('home', () => queryContent().find())
+
+const showRaw = ref(false);
+
 console.log(articles)
 </script>
